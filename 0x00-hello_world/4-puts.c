5ecfe16a405f4464eee42ce2836d9026d394e2aa
MichaelTwentytwo <michsmichs11@gmail.com>
2023-06-26 03:02:09 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - contains the main funtion
+ * puts - prints the text
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(string);
+
+	return (0);
+}
