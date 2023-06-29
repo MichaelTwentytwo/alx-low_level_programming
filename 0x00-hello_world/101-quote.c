8fd3e11ec78552254a76cbf5e16268aaa6303501
MichaelTwentytwo <michsmichs11@gmail.com>
2023-06-29 11:37:19 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+ * main - contains the main function
+ *
+ * Return: always 0 is sucess
+ */
+
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19'");
+
+	return (1);
+}
