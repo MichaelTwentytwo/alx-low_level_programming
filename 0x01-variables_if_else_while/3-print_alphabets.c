2d05cfbf5b14c35acddb95f5528766ad8eacf195
MichaelTwentytwo <michsmichs11@gmail.com>
2023-07-01 10:35:30 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,17 @@
 int main(void)
 {
 	char alpha = 'a';
+	char ALPHA = 'A';
 
 	while (alpha <= 'z')
 	{
 		putchar(alpha);
 		alpha++;
+	}
+	while (ALPHA <= 'Z')
+	{
+		putchar(ALPHA);
+		ALPHA++;
 	}
 	putchar('\n');
 
